fbdb36b1783fb54e54cba4102e8af7399f6117a1
Ortal <ortalcoh@gmail.com>
2019-10-01 10:49:46 +0300

    rfid init settings
    Signed-off-by: Ortal <ortalcoh@gmail.com>

---
+++ proj_inc/rfid_setting.h
@@ -0,0 +1,16 @@
+#ifndef RFID_SETTING_H
+#define RFID_SETTING_H
+
+
+
+// RFID
+#define GPIO_MOSI 12
+#define GPIO_MISO 13
+#define GPIO_SCLK 15
+#define GPIO_CS 14
+
+
+// RFID prototypes
+void rfidInit();
+
+#endif
---
+++ rfid_setting.c
@@ -0,0 +1,57 @@
+#include "driver/spi_slave.h"
+#include "esp_spi_flash.h"
+
+#include "proj_inc/rfid_setting.h"
+
+//Called after a transaction is queued and ready for pickup by master.
+void my_post_setup_cb(spi_slave_transaction_t *trans)
+{
+    return;
+}
+
+//Called after transaction is sent/received.
+void my_post_trans_cb(spi_slave_transaction_t *trans)
+{
+    //TO DO - change our yolo state according to what we received from RFID data
+
+	return;
+}
+
+
+void rfidInit()
+{
+	printf("*** RFID setting init function ***\n");
+	unsigned char rc;
+
+    //Configuration for the SPI bus
+    spi_bus_config_t buscfg={
+        .mosi_io_num=GPIO_MOSI,
+        .miso_io_num=GPIO_MISO,
+        .sclk_io_num=GPIO_SCLK
+    };
+
+    //Configuration for the SPI slave interface
+    spi_slave_interface_config_t slvcfg={
+        .mode=0,
+        .spics_io_num=GPIO_CS,
+        .queue_size=3,
+        .flags=0,
+        .post_setup_cb=my_post_setup_cb,
+        .post_trans_cb=my_post_trans_cb
+    };
+
+	//Enable pull-ups on SPI lines so we don't detect rogue pulses when no master is connected.
+    gpio_set_pull_mode(GPIO_MOSI, GPIO_PULLUP_ONLY);
+    gpio_set_pull_mode(GPIO_SCLK, GPIO_PULLUP_ONLY);
+    gpio_set_pull_mode(GPIO_CS, GPIO_PULLUP_ONLY);
+
+    //Initialize SPI slave interface
+    rc=spi_slave_initialize(HSPI_HOST, &buscfg, &slvcfg, 1);
+    printf("spi init status is %d", rc);
+    // TO DO - maybe we should add an assert that only rc is OK continue
+
+
+    //ret=spi_slave_transmit(HSPI_HOST, &t, portMAX_DELAY);
+
+}
+
